52c0d28d18d133fb3baed312cd86dd1170850391
D4KiR <d4kir92@gmail.com>
2022-09-13 10:11:12 +0200

    v1.0.2

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@
 if D4CENTAB == nil then
 	D4CENTAB = {}
 end
-D4CENTAB.name = "Centered |T132222/:16:16:0:0|t by |cFFAAAAFFD4KiR |T132115/:16:16:0:0|t"
+D4CENTAB.name = "Centered |T132222/:16:16:0:0|t by |cff3FC7EBD4KiR |T132115/:16:16:0:0|t"
 
 SetCVar("ScriptErrors", 1)
 
