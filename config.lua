f6389e9c872b9870466e257d705902da5729f78e
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-03-17 19:10:44 +0100

    v1.1.33

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ if D4CENTAB == nil then
 	D4CENTAB = {}
 end
 
-D4CENTAB.name = "Centered |T132222:16:16:0:0|t by |cff3FC7EBD4KiR |T132115:16:16:0:0|t"
+D4CENTAB.name = "Centered |T132222:16:16:0:0|t by |cff55d2ffD4KiR |T132115:16:16:0:0|t"
 function D4CENGetConfig(str, val)
 	local setting = val
 	D4CEN = D4CEN or {}
